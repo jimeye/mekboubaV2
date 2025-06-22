afe592ba915b5a03dc391e5ccdb7d4c46118d9ca
Jimmy Fellous <jimmyfellous@gmail.com>
2025-06-22 23:08:06 +0200

    Masquage des logos images sur mobile et réduction supplémentaire des boutons + Ajouter

--- app/components/HeroSliderNew.js
+++ app/components/HeroSliderNew.js
@@ -113,7 +113,7 @@ export default function HeroSliderNew() {
       <div className="relative w-full">
         {/* Logo mobile - visible seulement dans le slider */}
         {isInSlider && (
-          <div className="fixed top-[17%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 block landscape:hidden md:hidden">
+          <div className="fixed top-[17%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 hidden">
             <div className="relative w-32 h-32 md:w-52 md:h-52 scale-[1.46]">
               <Image
                 src="/images/logo ile.png"
